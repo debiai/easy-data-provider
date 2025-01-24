f6dc5be4f5f1b830c396e20be59a5690bd91de49
tom.mansion <tomansion@yahoo.fr>
2025-01-24 18:17:51 +0100

    Completed readme with example

--- examples/simple_project.py
+++ examples/simple_project.py
@@ -1,95 +1,66 @@
-# DebiAI Data Provider Python module
-
-[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-
-This DebiAI Data Provider Python module allows you to easily deploy your own data-provider through the data-provider API.
-
-A data-provider allows you to provide data to DebiAI so that no duplication of data is needed.
-
-[DebiAI Data-providers documentation](https://debiai.irt-systemx.fr/dataInsertion/dataProviders/)
-
-## Getting started
-
-### Installation
-
-Install `debiai_data_provider` with pip:
-
-```bash
-pip install debiai_data_provider
-```
-
-### Usage example
-
-Find out how to use the DebiAI Data Provider Python module in the [examples](examples) folder:
-
-- [simple_project.py](examples/simple_project.py) shows how to create a simple data-provider with a project
-
-#### Provide project metadata
-
-Create a Python Class representing your project:
-
-```python
+import pandas as pd
 from debiai_data_provider import DebiAIProject, DataProvider
 
+# This file is an example of a simple project
+# exposed with the DebiAI Data Provider python module.
+# It shows how to define a project structure,
+# provide project data and model results,
+# and define actions that can be performed on the project.
 
+# Project data
+PROJECT_DATA = pd.DataFrame(
+    {
+        "Data ID": ["image-1", "image-2", "image-3"],
+        "My context 1": ["A", "B", "C"],
+        "My context 2": [0.28, 0.388, 0.5],
+        "My groundtruth 1": [8, 7, 19],
+        "input": [100, 200, 128],
+    }
+)
+
+# Project model results
+MODEL_RESULTS = pd.DataFrame(
+    {
+        "sample_id": ["image-1", "image-1", "image-2", "image-2"],
+        "model": ["model_1", "model_2", "model_1", "model_2"],
+        "prediction": [10, 12, 8, 5],
+        "confidence": [0.8, 0.9, 0.7, 0.6],
+        "error": [2, 4, 1, -2],
+        "error_abs": [2, 4, 1, 2],
+    }
+)
+
+
+# Define a class that inherits from DebiAIProject
+# This class will be used to define the project structure
+# to gather the project samples and model results
+# and to define the actions that can be performed on the project
 class MyProject(DebiAIProject):
     creation_date = "2024-01-01"
 
+    # Project metadata
     def get_structure(self) -> dict:
         # This function will be called when the user
         # opens the project in the DebiAI interface
         # It serves to classify the project data structure
 
         return {
-            "Data ID": {"type": "text", "category": "id"},
-            "My context 1": {"type": "text", "category": "context"},
-            "My context 2": {"type": "number", "category": "context"},
-            "My groundtruth 1": {"type": "number", "category": "groundtruth"},
+            "My context 1": {
+                "type": "text",
+                "category": "context",
+                "group": "context",
+            },
+            "My context 2": {
+                "type": "number",
+                "category": "context",
+                "group": "context",
+            },
+            "My groundtruth 1": {
+                "type": "number",
+                "category": "groundtruth",
+            },
         }
 
-    def get_data(self) -> pd.DataFrame:
-        # This function will be called when the user
-        # wants to analyze data from your project
-        samples_df = pd.DataFrame(
-            {
-                "Data ID": ["image-1", "image-2", "image-3"],
-                "My context 1": ["A", "B", "C"],
-                "My context 2": [0.28, 0.388, 0.5],
-                "My groundtruth 1": [8, 7, 19],
-            }
-        )
-
-        return samples_df
-
-
-my_project = MyProject()
-```
-
-#### Start the server and link your data-provider with DebiAI
-
-Then, create an DataProvider object and add your project to it:
-
-```python
-provider = DataProvider()
-
-provider.add_project(my_project)
-
-# Finally, start the server
-provider.start_server()
-```
-
-Run the Python file and your project is now available through the DebiAI Data Provider API!
-
-To link your data-provider with DebiAI, you can follow our [Creation of a data provider guide](https://debiai.irt-systemx.fr/dataInsertion/dataProviders/quickStart.html)
-
-#### Provide project model results
-
-To prodive your model results to DebiAI, add the following method to your project class:
-
-```python
-class MyProject(DebiAIProject):
-
-    # Project metadata
     def get_results_structure(self) -> dict:
         # This function will be called when the user
         # opens the project in the DebiAI interface
@@ -111,6 +82,24 @@ class MyProject(DebiAIProject):
                 "group": "error",
             },
         }
+
+    # Project samples
+    def get_nb_samples(self) -> int:
+        # This function returns the number of samples in the project
+        return len(PROJECT_DATA)
+
+    def get_samples_ids(self) -> list[str]:
+        # This function returns the list of samples ids
+        return PROJECT_DATA["Data ID"].tolist()
+
+    def get_data(self, samples_ids: list[str]) -> pd.DataFrame:
+        # This function will be called when the user
+        # wants to analyze data from your project
+
+        # The function should return a pandas DataFrame
+        # containing the data corresponding to the samples_ids
+
+        return PROJECT_DATA[PROJECT_DATA["Data ID"].isin(samples_ids)]
 
     # Project model results
     def get_models(self) -> list[dict]:
@@ -162,28 +151,24 @@ class MyProject(DebiAIProject):
         ]
 
         return model_inferences
-```
 
-#### Provide project selections
+    # Project actions
+    def delete_project(self):
+        # This function will be called when the user
+        # wants to delete the project
+        print("Delete project called")
+        raise NotImplementedError
 
-TODO
 
-### Listen for user interactions
+# You can define multiple projects in the same file
+class MyProject2(DebiAIProject):
+    pass
 
-TODO
 
-#### Project deletion
+provider = DataProvider()
 
-TODO
+provider.add_project(MyProject())
+provider.add_project(MyProject2())
 
-#### Selection creation
-
-TODO
-
-#### Selection deletion
-
-TODO
-
-#### Model deletion
-
-TODO
+# Finally, start the server
+provider.start_server()
